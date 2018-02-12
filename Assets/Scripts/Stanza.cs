74fd1a48b8c1da0abbc4017e9984088dddebda1b
dhir_a <dhirakshit@gmail.com>
2018-02-12 12:36:23 +0530

    assets added
    Signed-off-by: dhir_a <dhirakshit@gmail.com>

---
+++ Assets/Scripts/Stanza.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stanza : MonoBehaviour {
+    public StanzaManager stanzaManager;
+    public List<TinkerText> tinkerTexts;
+    // Time delay at end of stanza during autoplay
+    public float endDelay;
+    private TinkerText mouseDownTinkerText;
+    private TinkerText mouseCurrentlyDownTinkerText;
+
+  
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public IEnumerator AutoPlay(TinkerText startingTinkerText = null)
+    {
+        int startingTinkerTextIndex = 0;
+
+        if (startingTinkerText != null)
+        {
+            startingTinkerTextIndex = tinkerTexts.IndexOf(startingTinkerText);
+        }
+
+        for (int i = startingTinkerTextIndex; i < tinkerTexts.Count; i++)
+        {
+            // delay according to timing data
+            //animation not integrated
+            //yield return new WaitForSeconds(tinkerTexts[i].GetAnimationDelay());
+            TinkerText t = tinkerTexts[i];
+            Animator anim = t.GetComponent<Animator>();
+            anim.speed = 1 / t.delayTime;
+            
+            // If we aren't on last word, delay before playing next word
+            if (i < tinkerTexts.Count - 1)
+            {
+                float pauseDelay = tinkerTexts[i + 1].GetStartTime() - tinkerTexts[i].GetEndTime();
+
+                anim.Play("textzoomout");
+                yield return new WaitForSeconds(t.delayTime/2);
+
+                //anim.SetTrigger("tapme");
+                anim.Play("textzoomin");
+                yield return new WaitForSeconds(t.delayTime / 2);
+
+                anim.speed = 1 / pauseDelay;
+                anim.Play("pausedelay");
+                //anim.SetTrigger("resume");
+                yield return new WaitForSeconds(pauseDelay);
+
+            }
+            else // Delay before next stanza
+            {
+               // anim.SetTrigger("tapme");
+               // anim.SetTrigger("resume");
+                anim.Play("textzoomout");
+                yield return new WaitForSeconds(t.delayTime / 2);
+
+                anim.Play("textzoomin");
+                yield return new WaitForSeconds(t.delayTime / 2);
+                if (endDelay != 0)
+                {
+                    anim.speed = 1 / endDelay;
+                    anim.Play("pausedelay");
+                }
+
+                yield return new WaitForSeconds(endDelay);
+
+            }
+
+            // Abort early?
+            if (stanzaManager.CancelAutoPlay())
+            {
+                yield break;
+            }
+        }
+
+        // Stop the coroutine
+        yield break;
+    }
+
+
+    public void OnDrag(TinkerText tinkerText)
+    {
+        tinkerText.OnDrag();
+        // and reassign it to currently down 
+        int currentTextIndex = tinkerTexts.IndexOf(tinkerText);
+
+        stanzaManager.RequestAutoPlay(this, tinkerTexts[currentTextIndex]);
+    }
+
+    public void OnMouseUp(TinkerText tinkerText)
+    {
+        // Assign this new one
+        mouseDownTinkerText = tinkerText;
+        // And signal the tinkerText 
+        tinkerText.OnMouseUp();
+    }
+  
+    public void OnMouseDown(TinkerText tinkerText)
+    {
+        // Assign this new one
+        mouseDownTinkerText = tinkerText;
+        // And signal the tinkerText 
+        tinkerText.OnMouseDown();
+    }
+}
---
+++ Assets/Scripts/TinkerGraphic.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TinkerGraphic : MonoBehaviour {
+    private Animator anim;
+    public TinkerText pairedtext1;
+    public TinkerText pairedtext2;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+   
+    public void OnMouseDown()
+    {
+        //if (pairedTinkerText != null)
+        //    TinkerText.pairedgraphic = true;
+
+        //Debug.Log("tinker graphic paired "+TinkerText.pairedgraphic);
+        graphicPlay();
+     
+    }
+    public void OnMouseUp()
+    {
+        graphicResume();
+        //TinkerText.pairedgraphic = false;
+    }
+    public void graphicPlay()
+    {
+        if (anim != null)
+        {
+            if (TinkerText.nooftaps < 3)
+            {
+                pairedtext1.clipPlay();
+                int i = TinkerText.nooftaps + 1;
+                anim.SetTrigger("crack" + i);
+
+            }
+            else
+            {
+               // TinkerText.nooftaps--;
+                if (pairedtext2 != null)
+                {
+                    pairedtext2.clipPlay();
+
+                }
+                anim.SetTrigger("crack3");
+
+                //SceneManager.LoadScene("Scene02");
+            }
+        }
+    }
+    public void graphicResume()
+    {
+        if (TinkerText.nooftaps < 3)
+        {
+            Debug.Log("graphic resumed "+TinkerText.nooftaps);
+            if (pairedtext1 != null)
+                pairedtext1.clipResume();
+            TinkerText.nooftaps++;
+
+        }
+        else
+        {
+            if (pairedtext2 != null)
+                pairedtext2.clipResume();
+            TinkerText.nooftaps = 0;
+        }
+    }
+    //public void clipPlay() {
+
+    //if (anim != null)
+    //{
+    //    //if (TinkerText.nooftaps < 2)
+    //    //{
+    //    //    int i = TinkerText.nooftaps + 1;
+    //        anim.SetTrigger("crack" + i);
+    //        //TinkerText.nooftaps++;
+    //        Debug.Log("tapcountgraphic: " + TinkerText.nooftaps);
+
+    //    //}
+    //    else
+    //    {
+    //        anim.SetTrigger("crack3");
+    //        SceneManager.LoadScene("Scene02");
+    //    }
+    //}
+
+
+    //  }
+
+
+
+}
