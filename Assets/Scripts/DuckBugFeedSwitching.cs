2d4cb683664d43fd8c60dbfef31152ff2bc71eef
Gurunatraj <p.gurunatraj@gmail.com>
2018-03-09 11:05:05 +0530

    Scene 16 bugs added
    Signed-off-by: Gurunatraj <p.gurunatraj@gmail.com>

--- Assets/Scripts/DuckBugFeedSwitching.cs
+++ Assets/Scripts/DuckBugFeedSwitching.cs
@@ -1,32 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DuckFeedSwitching : MonoBehaviour {
+public class DuckBugFeedSwitching : MonoBehaviour {
 
 	public GameObject duckIdle;
 	public GameObject duckChew;
 	public GameObject duckMouthOpen;
 	public GameObject bugInMouth;
+	public GameObject handHint;
 	public int bugCounter;
 	public SManager sceneManager;
+	public Text num;
+
 	// Use this for initialization
 	void Start () {
-		bugCounter = 0;
+		bugCounter = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//num.text = bugCounter.ToString();
 		
 	}
 
 	void OnTriggerEnter(Collider collider)
 	{   
-		Debug.Log ("collision called");
+		num.text = bugCounter.ToString ();
 		duckIdle.SetActive(false);  
 		duckChew.SetActive (false);
 		duckMouthOpen.SetActive (true); //mouth open
 		bugInMouth = collider.gameObject;
+		Debug.Log (bugInMouth);
 	}
 
 	void OnTriggerExit(Collider collider)
@@ -37,12 +43,24 @@ public class DuckFeedSwitching : MonoBehaviour {
 		duckChew.SetActive (false);
 	}
 
+	public void FeedDuckOnMouseDown()
+	{   
+		num.text = bugCounter.ToString ();
+	}
+
+	public void FeedDuckOnMouseCurrentlyDown()
+	{  
+		num.text = bugCounter.ToString ();
+	}
+
 	public void FeedDuckOnMouseUp()
-		{
+	{   num.text = "";
 		if (bugInMouth) {
 			Destroy (bugInMouth);
+			handHint.SetActive (false);
 			duckMouthOpen.SetActive (false);
 			duckChew.SetActive (true);
+
 			bugCounter++;
 			if (bugCounter == 8) {
 				sceneManager.NextScene ();
